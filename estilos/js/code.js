2bfa613b85db3cd143ee2df954dc32f463d5ea9c
alejandroqm23 <43794737+alejandroqm23@users.noreply.github.com>
2021-01-22 10:28:23 +0000

    Update code.js

--- estilos/js/code.js
+++ estilos/js/code.js
@@ -1,7 +1,7 @@
 function initialize() {
     var options = {atmosphere: true, center: [0, 0], zoom: 0};
     var earth = new WE.map('canvas', options);
-    WE.tileLayer('http://tileserver.maptiler.com/nasa/{z}/{x}/{y}.jpg', {
+    WE.tileLayer('https://webglearth.github.io/webglearth2-offline/{z}/{x}/{y}.jpg', {
       minZoom: 0,
       maxZoom: 5,
       attribution: 'NASA'
